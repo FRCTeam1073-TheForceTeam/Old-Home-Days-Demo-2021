3c226626a08ca17ccad1aab832b035ebe53ef82c
Joshua Robey <robey@frc1073.org>
2021-09-13 20:54:55 -0400

    Add the raspberry pi code
    Added directory and posted the raspberry pi code.
    NOTE: This is not the actual code but it is close.  The code is on the pi which I no longer have access to because the robocow is in Elizabeth's basement

---
+++ Raspberry Pi - Python and MP3 files/doitall.py
@@ -0,0 +1,45 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+import os
+Import random
+
+# Pin Definitons:
+servoPIN = 4
+servoButPIN = 15 
+mooButPIN = 17
+
+# GPIO setup:
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(servoButPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+GPIO.setup(mooButPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+p = GPIO.PWM(servoPIN, 50) # GPIO 4 for PWM with 50Hz
+p.start(5) # Initialization
+
+
+time.sleep(2)
+try:
+  while True:
+    if not GPIO.input(servoButPIN): # button is pressed:
+        print("servo but pressed, starting random countdown to pooping")
+        countdown = random.randrange(15, 60, 1)
+        time.sleep(countdown)
+        os.system('omxplayer  /home/pi/robocow/mp3_files/cow_moo.mp3 ')
+        countdown = random.randrange(15, 60, 1)
+        time.sleep(countdown)
+        os.system('omxplayer  /home/pi/robocow/mp3_files/fart-04.mp3 ')
+        print("cycle = 10")
+        p.ChangeDutyCycle(10)
+        time.sleep(3)
+        print("cycle = 5")
+        p.ChangeDutyCycle(5)
+        time.sleep(3)
+    if not GPIO.input(mooButPIN): # button is pressed:
+        print("moo but pressed")
+        os.system('omxplayer  /home/pi/robocow/mp3_files/cow_moo.mp3 ')
+        time.sleep(2)
+
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
